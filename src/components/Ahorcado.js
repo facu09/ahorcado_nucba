79d74ad7f4a054a5fb18ab0307d357498b2b0623
Facundo Cigliuti <fcigliuti@gmial.com>
2021-10-10 13:06:54 -0300

    3ro agrego visualizaciones de intentos by Franco

--- src/components/Ahorcado.js
+++ src/components/Ahorcado.js
@@ -9,10 +9,12 @@ const Ahorcado = () => {
     ///hanged-game/start
 
     const baseURL = 'https://back-sandbox.herokuapp.com/api';
+   
     const [letra, setLetra] = useState('');
+    const [respTry, setRespTry] = useState('');
 
+    //Funcion para empezar Juego
     const empezarJuego = async() => {
-
         try {
             const response = await fetch(`${baseURL}/hanged-game/start`, {
                 method: 'GET',
@@ -25,7 +27,9 @@ const Ahorcado = () => {
             const json = await response.json();
             console.log(json.data);
 
-            // setLetra('')
+            //blanqueo la varibale Letra para volver a empezar a jugar
+            setLetra('');
+            //FALTA: Tendria que ver como vacio el input FALTA
 
         } catch (error){
             alert(error);
@@ -34,8 +38,8 @@ const Ahorcado = () => {
 
     const handleOnChange = (e) => {
         console.log (e.target.value);
+        //actualizo la variable de estado letra
         setLetra(e.target.value);
-       
     }
 
     const mandarLetraOPalabra = async () => {
@@ -52,7 +56,9 @@ const Ahorcado = () => {
             
             //le agrego que console la respuesta del Post
             const json = await response.json();
-            console.log(json.data);
+            console.log('Consoleo objeto json.data', json.data);
+            //meto la respuesta en una varible de estado para mostrarla por pantalla
+            setRespTry(json.data);
 
         } catch(error){
             alert(error);
@@ -65,11 +71,30 @@ const Ahorcado = () => {
         <div>
             <button onClick= {empezarJuego}>Empezar Juego</button>
         
-            <input onChange={handleOnChange}  type = 'text' placeholder = 'letra o palabra'  />
+            <input onChange={handleOnChange} type = 'text' placeholder = 'letra o palabra'  />
 
             <button onClick= {mandarLetraOPalabra}>Mandar Letra</button>
         
-        
+            {/* Tomado de Franco */}
+            <div>
+
+                <strong>{`Intentos: ${respTry.attempsMade} de ${respTry.totalAttemps}`}</strong>
+
+                <p>{`Letras erroneas: ${respTry.wrongLetters}`}</p>
+                <p>{`Cantidad de letras: ${respTry.totalWords}`}</p>
+
+                <p style={{color: "red", 
+                    border: "solid 2px black", 
+                    margin: "0 30% 0 30%", 
+                    letterSpacing: "10px",
+                    padding: "15px"}}>
+                    {`${respTry.matcheds}`}
+                </p>
+
+                <strong>{respTry.message}</strong>
+
+            </div>
+
         </div>
     )
 }
